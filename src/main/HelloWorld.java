047915b9e9fc7aeb9d703fdb0c6523ba23d67450
Vũ Thanh Sang <sangthanh980@gmail.com>
2015-11-22 21:03:13 +0700

    [SangVT] Thêm lỗi cho hàm main

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		Add a = new Add();
 		int result = a.Add(20, 10);
 		System.out.println(result);
-		System.out.println(Sub.sub(12,6));
+		System.out.println(Sub.sub(12,6,3));
 	}
 	
 
