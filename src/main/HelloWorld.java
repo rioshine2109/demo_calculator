1baf21d9c8081d5db9a64d4e99bdd2a4e45108e8
Vũ Thanh Sang <sangthanh980@gmail.com>
2015-11-22 21:32:43 +0700

    [SANGVT] sua loi

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -9,13 +9,7 @@ public class HelloWorld {
 		int result = a.Add(20, 10);
 		System.out.println(result);
 		
-		Mutil m = new Mutil();
-		int result2 = m.mutil(2, 5);
-		System.out.println(result2);
-		
-		
-		
-		int result3 = Sub.subc(2, 5);
+		int result3 = Sub.sub(2, 5);
 		System.out.println(result3);
 	}
 	
