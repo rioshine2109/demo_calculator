0488b445a06d4e852beceb6c143a743fd691d4ed
RioShine <rioshine1994@gmail.com>
2015-11-22 21:06:41 +0700

    add Mutil

---
+++ src/calculator/Mutil.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class Mutil {
+	public int mutil(int a, int b)
+	{
+		return a*b;
+	}
+
+}
--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -8,7 +8,10 @@ public class HelloWorld {
 		Add a = new Add();
 		int result = a.Add(20, 10);
 		System.out.println(result);
-		System.out.println(Sub.sub(12,6,3));
+		
+		Mutil m = new Mutil();
+		int result2 = m.mutil(2, 5);
+		System.out.println(result2);
 	}
 	
 
